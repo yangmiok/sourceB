4882986b4666df8fa1d71872922dff456794e5cf
mengmanzbh <71998853+Polka-DeFi@users.noreply.github.com>
2021-07-16 21:13:46 +0800

    1

---
+++ auto.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import paramiko
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+ssh = paramiko.SSHClient()
+
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
+ssh.connect(hostname='192.168.38.38', port=22, username='root', password='xxx')
+
+
+stdin, stdout, stderr = ssh.exec_command('/root/filecoin/cmd/lotus-miner info')
+
+res, err = stdout.read(), stderr.read()
+result = res if res else err
+print(result.decode())
